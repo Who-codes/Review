71fd87a3a222084a97bd6491726963e84b1e813f
Sulav <sulavshrestha023@gmail.com>
2022-05-19 16:46:33 +0545

    new funtion for buttons as a final touch

--- src/Review.jsx
+++ src/Review.jsx
@@ -6,16 +6,36 @@ const Review = () => {
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = people[index];
 
+  const checkNumber = (number) => {
+    if (number > people.length - 1) {
+      return 0;
+    }
+    if (number < 0) {
+      return people.length - 1;
+    }
+    return number;
+  };
+
   const prevPerson = () => {
-    setIndex((prev) => (index ? prev - 1 : people.length - 1));
+    setIndex((prev) => {
+      let newIndex = index - 1;
+      return checkNumber(newIndex);
+    });
   };
 
   const nextPerson = () => {
-    setIndex((prev) => (index === people.length - 1 ? 0 : prev + 1));
+    setIndex((prev) => {
+      let newIndex = index + 1;
+      return checkNumber(newIndex);
+    });
   };
 
   const generateRandom = () => {
-    setIndex(Math.floor(Math.random() * people.length));
+    let randomNumber = Math.floor(Math.random() * people.length);
+    if (randomNumber === index) {
+      randomNumber += 1;
+    }
+    setIndex(checkNumber(randomNumber));
   };
 
   return (
